24c0e9dd3c47be8df75c7d981d3ad525bb25c5e6
Robert Mayer <rwlmayer@gmail.com>
2016-12-03 20:47:50 +0100

    Fixed #16

--- WebApp-B2C-DotNet/App_Start/BundleConfig.cs
+++ WebApp-B2C-DotNet/App_Start/BundleConfig.cs
@@ -28,6 +28,7 @@ namespace WebApp_OpenIDConnect_DotNet_B2C
 
             bundles.Add(new StyleBundle("~/Content/appcss").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.yeti.min.css",
                       "~/Content/toastr.min.css",
                       "~/css/font-awesome.min.css",
                       "~/Content/css/select2.min.css",
