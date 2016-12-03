867898346afc10237c3e518acb2b3b952d095fc8
Robert Mayer <rwlmayer@gmail.com>
2016-12-03 21:16:47 +0100

    Added SeasonEntity

---
+++ Pingis.Models/SeasonEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pingis.Models
+{
+    public class SeasonEntity : TableEntity
+    {
+        public SeasonEntity(Guid seasonId, string name, DateTimeOffset startDate)
+        {
+            this.PartitionKey = seasonId.ToString();
+            this.RowKey = name;
+            this.StartDate = startDate;
+        }
+
+        public SeasonEntity() {
+        }
+
+        public string SeasonId { get { return this.RowKey;  } }
+        public string Name { get { return this.PartitionKey; } }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+    }
+}
