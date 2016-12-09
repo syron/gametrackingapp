e4411153d0c7e7b4e03f8f59117832530c557372
Robert Mayer <rwlmayer@gmail.com>
2016-12-09 12:49:58 +0100

    Added GameMode Entity - #72

---
+++ Pingis.Models/GameModeEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pingis.Models
+{
+    public class GameModeEntity : TableEntity
+    {
+        public GameModeEntity(string gameModeId, string created, string name, string description)
+        {
+            this.PartitionKey = gameModeId;
+            this.RowKey = created;
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public GameModeEntity()
+        {
+            
+        }
+
+        public string GameModeId { get { return this.PartitionKey; } }
+        public string Created { get { return this.RowKey; } }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
