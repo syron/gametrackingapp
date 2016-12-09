f1f9e2bcd230f34a286edc8f685dace5b22e5469
Robert Mayer <rwlmayer@gmail.com>
2016-12-09 12:55:53 +0100

    Added DAL for game modes - #73

---
+++ Pingis.DAL/GameModes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using Pingis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pingis.DAL
+{
+    public class GameModes : DALBase
+    {
+        CloudTable table = null;
+
+        public GameModes() : base()
+        {
+            table = tableClient.GetTableReference("GameModes");
+            table.CreateIfNotExists();
+        }
+
+        public List<GameModeEntity> GetAll()
+        {
+            TableQuery<GameModeEntity> query = new TableQuery<GameModeEntity>();
+            return table.ExecuteQuery(query).ToList();
+        }
+        
+        public GameModeEntity GetByGameModeId(string gameModeId)
+        {
+            TableQuery<GameModeEntity> query = new TableQuery<GameModeEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, gameModeId));
+            return table.ExecuteQuery(query).FirstOrDefault();
+        }
+    }
+}
